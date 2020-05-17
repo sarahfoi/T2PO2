8224f6c4c87fe375458d3e332ac66876fb248837
João Frediani <joaoorff@gmail.com>
2020-05-17 16:28:49 -0300

    home feio

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,9 +5,28 @@ import Header from '../../components/Header';
 
 export default function Home(){
     return(
-        <div class='home-container'>
-            <p>Olá, esse é o trabalho de P.O. II.</p>
+        <div class='home'>
+                    <h1>Trabalho de Pesquisa Operacional II </h1>
+                    
+                    <h2>
+                        Este trabalho objetiva a resolução de problemas envolvendo funções não-lineares pelos métodos:
+                    </h2>
 
+                    <ul class='metodo' style={{listStyleType: 'none'}} >
+                        <li>-Busca Uniforme</li> 
+                        <li>-Busca Dicotômica</li>
+                        <li>-Seção Aurea</li> 
+                        <li>-Busca de Fibonacci</li> 
+                        <li>-Bisseção</li> 
+                        <li>-Newton</li> 
+                    </ul>
+
+                    <ul class='alunos' style={{listStyleType: 'none'}} >
+                        <li>Trabalho realizado por:</li>
+                        <li>João Otavio Rodrigues Ferreira Frediani RA:181020289</li>
+                        <li>Mauricio Scarelli Arantes RA:181020904</li>
+                        <li>Sarah Campos RA:181023822</li>
+                    </ul>
         </div>
     );
 }
