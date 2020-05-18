421e09fa1b6f6e58770a8602e5b03916716cf725
Scarelli <mauarants@gmail.com>
2020-05-17 22:08:57 -0300

    Seção Aurea corrigida

--- src/methods/Aurea/index.js
+++ src/methods/Aurea/index.js
@@ -5,20 +5,20 @@ export default async({f,a,b,e}) => new Promise((resolve,reject) =>{
     let y=b;
     let fm;
     let fl;
-    let micra = x + (y-x)*alfa;
-    let lambda= x + (y-x)*beta;
-    while ((b-a)>e) {
+    let micra = x + (y-x)*beta;
+    let lambda= x + (y-x)*alfa;
+    while ((x-y)>e) {
         fm=f(micra);
         fl=f(lambda);
         if(fm>fl){
             x=micra;
             micra=lambda;
-            lambda= x + (y-x)*beta;
+            lambda= x + (y-x)*alfa;
         }
         else {
             y=lambda;
             lambda=micra;
-            micra = x + (y-x)*alfa;
+            micra = x + (y-x)*beta;
         }
     }
     resolve( (x+y)/2);
